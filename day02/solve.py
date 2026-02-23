d1d906813e3b85c41cdc65fe73ca9901b20b51cd
Mike Day <mikeday370146@gmail.com>
2026-02-23 12:53:08 -0500

    day 2 done

--- day02/solve.py
+++ day02/solve.py
@@ -2,34 +2,41 @@ import common
 
 test_1_input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 
-def is_invalid_id(id):
+def is_invalid_part_1(id):
     s = str(id)
- 
-    # invalid ids have even length
     if len(s) % 2 != 0:
         return False
-    
-    # invalid ids have the first half matching the last
     half = int(len(s)/2)
     return s[:half] == s[half:]
 
-def get_invalid_id_sum(range_from, range_to):
+def is_invalid_part_2(id):
+    s = str(id)
+    for prefix_len in range(1, int(len(s)/2)+1):
+        prefix = s[:prefix_len]
+        if len(s) % prefix_len != 0:
+            continue
+        repeat_count = int(len(s) / prefix_len)
+        if s == prefix * repeat_count:
+            return True
+    return False
+
+def solve_part(input, is_invalid):
     invalid_sum = 0
-    for id in range(range_from, range_to+1):
-        if is_invalid_id(id):
-            invalid_sum += id
+    for raw_range in input.split(','):
+        range_from, range_to = [int(x) for x in raw_range.split('-')]
+        for id in range(range_from, range_to+1):
+            if is_invalid(id):
+                invalid_sum += id
     return invalid_sum
 
-def solve_part_1(input):
-    invalid_sum = 0
-    for range in input.split(','):
-        range_from, range_to = [int(x) for x in range.split('-')]
-        invalid_sum += get_invalid_id_sum(range_from, range_to)
-    return invalid_sum
-
-test_1_result = solve_part_1(test_1_input)
+test_1_result = solve_part(test_1_input, is_invalid_part_1)
 print("Test 1: ", "Pass" if test_1_result == 1227775554 else f"!! FAIL !! ({test_1_result})")
 
-part_1_result =  solve_part_1(common.read_input())
+test_2_result = solve_part(test_1_input, is_invalid_part_2)
+print("Test 2: ", "Pass" if test_2_result == 4174379265 else f"!! FAIL !! ({test_2_result})")
+
+part_1_result =  solve_part(common.read_input(), is_invalid_part_1)
 print("Part 1:", part_1_result, "|", "Pass" if part_1_result == 17077011375 else f"!! FAIL !! ({part_1_result})")
-                        
+
+part_2_result =  solve_part(common.read_input(), is_invalid_part_2)
+print("Part 2:", part_2_result, "|", "Pass" if part_2_result == 36037497037 else f"!! FAIL !! ({part_2_result})")
