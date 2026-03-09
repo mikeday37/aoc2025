867072f28bcf549d59bd2c30a1a972f741a69242
Mike Day <mikeday370146@gmail.com>
2026-03-09 17:12:11 -0400

    day 4 done

--- day04/solve.py
+++ day04/solve.py
@@ -1,17 +1,5 @@
 from common import *
 
-_example_input = """\
-..@@.@@@@.
-@@@.@.@.@@
-@@@@@.@.@@
-@.@@@@..@.
-@@.@@@@.@@
-.@@@@@@@.@
-.@.@.@.@@@
-@.@@@.@@@@
-.@@@@@@@@.
-@.@.@@@.@."""
-
 def convert_to_roll_set(input: str):
     rolls = set()
     for y, line in enumerate(input.splitlines()):
@@ -19,16 +7,6 @@ def convert_to_roll_set(input: str):
             if value == "@":
                 rolls.add((x, y))
     return rolls
-
-test(convert_to_roll_set, {(1,2)}, """\
-..
-..
-.@""")
-
-test(convert_to_roll_set, {(1,1), (4,1), (3,2)}, """\
-......
-.@..@.
-...@..""")
 
 def count_neighboring_rolls(x, y, rolls):
     n = 0
@@ -40,26 +18,63 @@ def count_neighboring_rolls(x, y, rolls):
                 n += 1
     return n
 
-_example_rolls = convert_to_roll_set(_example_input)
-test(count_neighboring_rolls, 7, 3, 1, _example_rolls)
-test(count_neighboring_rolls, 4, 1, 0, _example_rolls)
-test(count_neighboring_rolls, 8, 4, 4, _example_rolls)
-
 def count_accessible_rolls(rolls):
-#    return sum(1 for x, y in rolls if count_neighboring_rolls(x, y, rolls) < 4)
     return sum(count_neighboring_rolls(x, y, rolls) < 4 for x, y in rolls)
 
-test(count_accessible_rolls, 13, _example_rolls)
+def count_removable_rolls(initial_rolls):
+    n = 0
+    rolls = initial_rolls.copy()
+    while True:
+        to_remove = [(x, y) for x, y in rolls if count_neighboring_rolls(x, y, rolls) < 4]
+        if len(to_remove) == 0:
+            break
+        for roll in to_remove:
+            rolls.remove(roll)
+            n += 1
+    return n
 
 def solve_part_1():
     rolls = convert_to_roll_set(read_input())
     return count_accessible_rolls(rolls)
 
-test(solve_part_1, 1445)
+def solve_part_2():
+    rolls = convert_to_roll_set(read_input())
+    return count_removable_rolls(rolls)
 
 print("Part 1:", solve_part_1())
+print("Part 2:", solve_part_2())
 
-#def count_removable_rolls(rolls):
- #   state = rolls.copy()
-  #  do:
-    
+# --- tests ---
+
+test(convert_to_roll_set, {(1,2)}, """\
+..
+..
+.@""")
+
+test(convert_to_roll_set, {(1,1), (4,1), (3,2)}, """\
+......
+.@..@.
+...@..""")
+
+_example_rolls = convert_to_roll_set("""\
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.""")
+
+test(count_neighboring_rolls, 7, 3, 1, _example_rolls)
+test(count_neighboring_rolls, 4, 1, 0, _example_rolls)
+test(count_neighboring_rolls, 8, 4, 4, _example_rolls)
+
+test(count_accessible_rolls, 13, _example_rolls)
+
+test(count_removable_rolls, 43, _example_rolls)
+
+test(solve_part_1, 1445)
+test(solve_part_2, 8317)
