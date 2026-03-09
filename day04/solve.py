b0cd118c216e5dc645e5fcc4b655965254a62834
Mike Day <mikeday370146@gmail.com>
2026-03-09 16:44:51 -0400

    refactored day 4 part 1 to use a set of roll coordinates in prep for part 2

--- day04/solve.py
+++ day04/solve.py
@@ -12,49 +12,49 @@ _example_input = """\
 .@@@@@@@@.
 @.@.@@@.@."""
 
-def count_neighbors(x: int, y: int, diagram: list[str], width: int, height: int) -> int:
+def convert_to_roll_set(input: str):
+    rolls = set()
+    for y, line in enumerate(input.splitlines()):
+        for x, value in enumerate(line):
+            if value == "@":
+                rolls.add((x, y))
+    return rolls
+
+test(convert_to_roll_set, {(1,2)}, """\
+..
+..
+.@""")
+
+test(convert_to_roll_set, {(1,1), (4,1), (3,2)}, """\
+......
+.@..@.
+...@..""")
+
+def count_neighboring_rolls(x, y, rolls):
     n = 0
-    # interval to check will be [start, end)
-    start_x = x - 1 if x > 0 else 0
-    start_y = y - 1 if y > 0 else 0
-    end_x = x + 2 if x < width - 1 else width
-    end_y = y + 2 if y < height - 1 else height
-    for at_y in range(start_y, end_y):
-        for at_x in range(start_x, end_x):
-            if (at_y == y and at_x == x):
+    for at_x in range(x - 1, x + 2):
+        for at_y in range(y - 1, y + 2):
+            if at_x == x and at_y == y:
                 continue
-            if diagram[at_y][at_x] == "@":
+            if (at_x, at_y) in rolls:
                 n += 1
     return n
 
-test(count_neighbors, 8, 1, 1, ["@@@","@@@","@@@"], 3, 3)
-test(count_neighbors, 7, 1, 1, ["@@@","@@.","@@@"], 3, 3)
-test(count_neighbors, 1, 1, 1, ["...","..@","..."], 3, 3)
-test(count_neighbors, 3, 1, 0, ["@@","@@"], 2, 2)
-test(count_neighbors, 3, 0, 1, ["@@","@@"], 2, 2)
-test(count_neighbors, 0, 0, 0, ["@"], 1, 1)
+_example_rolls = convert_to_roll_set(_example_input)
+test(count_neighboring_rolls, 7, 3, 1, _example_rolls)
+test(count_neighboring_rolls, 4, 1, 0, _example_rolls)
+test(count_neighboring_rolls, 8, 4, 4, _example_rolls)
 
-def count_accessible_rolls(diagram_raw: str) -> int:
-    n = 0
-    diagram = diagram_raw.splitlines()
-    width = len(diagram[0])
-    height = len(diagram)
-    for y in range(0, height):
-        for x in range(0, width):
-            if diagram[y][x] != "@":
-                continue
-            neighbors = count_neighbors(x, y, diagram, width, height)
-            if neighbors < 4:
-                n += 1
-    return n
+def count_accessible_rolls(rolls):
+    return sum(1 for x, y in rolls if count_neighboring_rolls(x, y, rolls) < 4)
 
-test(count_accessible_rolls, 13, _example_input)
+test(count_accessible_rolls, 13, _example_rolls)
 
 def solve_part_1():
-    return count_accessible_rolls(read_input())
+    rolls = convert_to_roll_set(read_input())
+    return count_accessible_rolls(rolls)
 
 test(solve_part_1, 1445)
 
 print("Part 1:", solve_part_1())
-
 
