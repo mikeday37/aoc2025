d1d44f74fec67c4a33fe56481d1455cce4df46f2
Mike Day <mikeday370146@gmail.com>
2026-03-10 14:25:17 -0400

    move tests to end for clarity of solution

--- day05/solve.py
+++ day05/solve.py
@@ -1,26 +1,11 @@
 from common import *
 
-_example = """\
-3-5
-10-14
-16-20
-12-18
-
-1
-5
-8
-11
-17
-32"""
-
 def parse_input(raw_input):
     lines = raw_input.splitlines()
     blank_at = lines.index("")
     ranges = [(int(a), int(b)) for a, b in (line.split('-') for line in lines[:blank_at])]
     ingredients = [int(a) for a in lines[blank_at + 1:]]
     return (ranges, ingredients)
-
-test(parse_input, ([(3, 5), (10, 14), (16, 20), (12, 18)], [1, 5, 8, 11, 17, 32]), _example)
 
 def count_fresh_ingredients(ranges, ingredients):
     n = 0
@@ -31,14 +16,8 @@ def count_fresh_ingredients(ranges, ingredients):
                 break
     return n
 
-test(count_fresh_ingredients, 3, *parse_input(_example))
-
 def solve_part_1():
     return count_fresh_ingredients(*parse_input(read_input()))
-
-print("Part 1:", solve_part_1())
-
-test(solve_part_1, 726)
 
 def simplify_ranges(initial_ranges):
     ranges = initial_ranges.copy()
@@ -54,21 +33,40 @@ def simplify_ranges(initial_ranges):
     result.append((last_a, last_b))
     return result
 
+def count_in_ranges(ranges):
+    return sum(1 + b - a for a, b in simplify_ranges(ranges))
+
+def solve_part_2():
+    return count_in_ranges(simplify_ranges(parse_input(read_input())[0]))
+
+_example = """\
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32"""
+
+test(parse_input, ([(3, 5), (10, 14), (16, 20), (12, 18)], [1, 5, 8, 11, 17, 32]), _example)
+
+test(count_fresh_ingredients, 3, *parse_input(_example))
+
 test(simplify_ranges, [(1,3)],          [(1,3), (2,2)])
 test(simplify_ranges, [(1,3), (4,5)],   [(1,3), (4,5)])
 test(simplify_ranges, [(1,5)],          [(1,3), (2,5)])
 test(simplify_ranges, [(3,5), (10,20)], parse_input(_example)[0])
 
-def count_in_ranges(ranges):
-    return sum(1 + b - a for a, b in simplify_ranges(ranges))
-
 test(count_in_ranges, 3, [(1,2), (3,3)])
 test(count_in_ranges, 10, [(1,2), (11,18)])
 test(count_in_ranges, 14, simplify_ranges(parse_input(_example)[0]))
 
-def solve_part_2():
-    return count_in_ranges(simplify_ranges(parse_input(read_input())[0]))
-
-print("Part 2:", solve_part_2())
-
+test(solve_part_1, 726)
 test(solve_part_2, 354226555270043)
+
+print("Part 1:", solve_part_1())
+print("Part 2:", solve_part_2())
