edd0fac8a16ead6227c7790ba9d4aeb4741f54cb
Mike Day <mikeday370146@gmail.com>
2026-03-10 16:48:33 -0400

    cleanup: simplify day 2 tests

--- day02/solve.py
+++ day02/solve.py
@@ -36,8 +36,7 @@ print("Part 2:", solve_part(_input, is_invalid_part_2))
 
 _example = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 
-test(lambda: solve_part(_example, is_invalid_part_1), 1227775554)
-test(lambda: solve_part(_example, is_invalid_part_2), 4174379265)
-
-test(lambda: solve_part(_input, is_invalid_part_1), 17077011375)
-test(lambda: solve_part(_input, is_invalid_part_2), 36037497037)
+test(solve_part, 1227775554, _example, is_invalid_part_1)
+test(solve_part, 4174379265, _example, is_invalid_part_2)
+test(solve_part, 17077011375, _input, is_invalid_part_1)
+test(solve_part, 36037497037, _input, is_invalid_part_2)
