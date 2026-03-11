194a1301d7ab0af4f4835b1a50f84595515a953c
Mike Day <mikeday370146@gmail.com>
2026-03-11 17:03:55 -0400

    solved day 8 part 1 with much faster approach

--- day08/solve.py
+++ day08/solve.py
@@ -1,10 +1,65 @@
 from common import *
-from math import sqrt
+from math import sqrt, prod
 from collections import defaultdict
 from time import perf_counter
 
 def parse_input(input):
     return tuple(tuple(int(a) for a in line.split(',')) for line in input.splitlines())
+
+def find_n_closest_pairs(n, points):
+    pairs = []
+    for a in range(0, len(points) - 1):
+        for b in range(a + 1, len(points)):
+            (x1, y1, z1), (x2, y2, z2) = points[a], points[b]
+            dist = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+            pairs.append((a, b, dist))
+    pairs.sort(key = lambda x: x[2])
+    return [(a,b) for (a,b,dist) in pairs][:n]
+
+def collect_circuits(pairs):
+    last_circuit_id = 0
+    circuits = defaultdict(set)
+    point_to_circuit = dict()
+    for a, b in pairs:
+        if a not in point_to_circuit and b not in point_to_circuit:
+            # new circuit
+            last_circuit_id += 1
+            circuits[last_circuit_id].add(a)
+            circuits[last_circuit_id].add(b)
+            point_to_circuit[a] = last_circuit_id
+            point_to_circuit[b] = last_circuit_id
+        elif a in point_to_circuit and b not in point_to_circuit:
+            # add b to a's circuit
+            id = point_to_circuit[a]
+            point_to_circuit[b] = id
+            circuits[id].add(b)
+        elif b in point_to_circuit and a not in point_to_circuit:
+            # add a to b's circuit
+            id = point_to_circuit[b]
+            point_to_circuit[a] = id
+            circuits[id].add(a)
+        else:
+            # they're both in circuits, so merge unless already the same circuit
+            a_id, b_id = point_to_circuit[a], point_to_circuit[b]
+            if a_id == b_id:
+                continue
+            dest_id, source_id = (a_id, b_id) if len(circuits[a_id]) >= len(circuits[b_id]) else (b_id, a_id)
+            for p in circuits[source_id]:
+                circuits[dest_id].add(p)
+                point_to_circuit[p] = dest_id
+            del circuits[source_id]
+    return set(tuple(sorted(point_set)) for point_set in circuits.values()) # easiest structure for test comparison
+
+def solve_part_1(top_n, input):
+    circuits = collect_circuits(find_n_closest_pairs(top_n, parse_input(input)))
+    sizes = [len(circuit) for circuit in circuits]
+    sizes.sort(reverse = True)
+    return prod(sizes[:3])
+
+print("Part 1:", solve_part_1(1000, read_input()))
+
+
+# ==== Tests ====
 
 _example = """\
 162,817,812
@@ -28,41 +83,22 @@ _example = """\
 984,92,344
 425,690,689"""
 
-def find_closest_pair(points, exclude = set()):
-    closest_dist = float(1000 ** 3)
-    closest = -1
-    for a, (x1, y1, z1) in enumerate(points):
-        for b, (x2, y2, z2) in enumerate(points):
-            if a != b and not ((a, b) in exclude or (b, a) in exclude):
-                dist = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
-                if dist < closest_dist:
-                    closest_dist = dist
-                    closest = (a, b) if a <= b else (b, a)
-    return closest
+def find_n_closest_pairs_test_helper(n, points):
+    return [(points[a], points[b]) for (a, b) in find_n_closest_pairs(n, points)]
 
-def find_closest_pair_test_helper(points, exclude = set()):
-    closest = find_closest_pair(points, exclude)
-    return closest, points[closest[0]], points[closest[1]]
+test(find_n_closest_pairs_test_helper,
+    [
+        ((162, 817, 812), (425, 690, 689)),
+        ((162, 817, 812), (431, 825, 988)),
+        ((906, 360, 560), (805, 96, 715))
+    ],
+    3, parse_input(_example))
 
-test(find_closest_pair_test_helper, ((0, 19), (162,817,812), (425,690,689)), parse_input(_example))
-test(find_closest_pair_test_helper, ((0, 7), (162,817,812), (431,825,988)), parse_input(_example), {(0, 19)})
-test(find_closest_pair_test_helper, ((2, 13), (906,360,560), (805,96,715)), parse_input(_example), {(0, 19), (0, 7)})
+test(collect_circuits, {(1,2,3)}, [(1,2),(2,3)])
+test(collect_circuits, {(1,2),(3,4)}, [(1,2),(3,4)])
+test(collect_circuits, {(1,2,3),(10,11,12,13)}, [(1,2),(2,3),(10,11),(12,13),(10,12)])
+test(collect_circuits, {(1,2,3,10,11,12,13)}, [(1,2),(2,3),(10,11),(12,13),(10,12),(2,12)])
+test(collect_circuits, {(1,2,3)}, [(1,2),(3,2)])
 
-def find_n_closest_pairs(n, points):
-    exclude = set()
-    for i in range(0, n):
-        exclude.add(find_closest_pair(points, exclude))
-    return exclude
-
-test(find_n_closest_pairs, {(0, 7), (0, 19), (2, 13)}, 3, parse_input(_example))
-
-
-start = perf_counter()
-brute_force_result = find_n_closest_pairs(1000, parse_input(read_input()))
-duration = perf_counter() - start
-
-print(f"Brute Force Solution: {duration:.6f} seconds")
-print(brute_force_result)
-print(duration)
-
-# note: brute forcing the full input took 210 seconds.
+test(solve_part_1, 40, 10, _example)
+test(solve_part_1, 67488, 1000, read_input())
