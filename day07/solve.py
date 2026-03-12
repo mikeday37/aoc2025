922f1a35c146b7835b394ba33a917a5a27995a3b
Mike Day <mikeday370146@gmail.com>
2026-03-12 10:08:26 -0400

    revisited day 7 part 2's original recursive solution, made it viable with functools.cache, added timing comparison

--- day07/solve.py
+++ day07/solve.py
@@ -3,7 +3,7 @@ from common import *
 def parse_input(input):
     lines = [line for line in input.splitlines() if not all(char == '.' for char in line)]
     start = lines[0].index('S')
-    splitter_rows = [[index for index, char in enumerate(line) if char == '^'] for line in lines[1:]]
+    splitter_rows = tuple(tuple(index for index, char in enumerate(line) if char == '^') for line in lines[1:])
     return start, splitter_rows
 
 def solve(input):
@@ -68,15 +68,15 @@ _example = """\
 .^.^.^.^.^...^.
 ..............."""
 
-test(parse_input, (7, [
-        [7],
-        [6, 8],
-        [5, 7, 9],
-        [4, 6, 10],
-        [3, 5, 9, 11],
-        [2, 6, 12],
-        [1, 3, 5, 7, 9, 13]
-    ]), _example)
+test(parse_input, (7, (
+        (7, ),
+        (6, 8),
+        (5, 7, 9),
+        (4, 6, 10),
+        (3, 5, 9, 11),
+        (2, 6, 12),
+        (1, 3, 5, 7, 9, 13)
+    )), _example)
 
 test(count_splits, 21, _example)
 
@@ -113,3 +113,50 @@ test(count_timelines, 40, _example)
 
 verify_known_answer(count_splits, 1553, read_input())
 verify_known_answer(count_timelines, 15811946526915, read_input())
+
+
+# ==== Alternate Solution for Part 2 ====
+
+print("-- Alternate Part 2 --")
+
+from functools import cache
+
+@cache
+def count_timelines2(y, x, splitter_rows):
+    if y >= len(splitter_rows):
+        return 1
+    if x in splitter_rows[y]:
+        return count_timelines2(y + 1, x - 1, splitter_rows) + count_timelines2(y + 1, x + 1, splitter_rows)
+    else:
+        return count_timelines2(y + 1, x, splitter_rows)
+    
+test(count_timelines2, 40, 0, *parse_input(_example))
+test(count_timelines2, 10, 0, *parse_input(_example2))
+verify_known_answer(count_timelines2, 15811946526915, 0, *parse_input(read_input()))
+print(count_timelines2.cache_info())
+
+
+# ==== Comparing the Two Approaches ====
+
+import timeit
+
+input = read_input()
+def count_timelines2_fresh(input):
+    # timing would be trivial and non-representative without clearing the cache before each test
+    count_timelines2.cache_clear()
+    return count_timelines2(0, *parse_input(input))
+tries = 100
+print(f"-- Part 2 Timing (tries = {tries}) --")
+recursive_duration = timeit.timeit(lambda: count_timelines2_fresh(input), number=tries) / tries
+non_recursive_duration = timeit.timeit(lambda: count_timelines(input), number=tries) / tries
+print(f"Cached Recurisve Approach: {recursive_duration:.6f} seconds")
+print(f"Non-Recursive Approach: {non_recursive_duration:.6f} seconds")
+if recursive_duration > non_recursive_duration:
+    print(f"Non-Recursive is faster by a factor of {recursive_duration/non_recursive_duration:.2f} x")
+else:
+    print(f"Recursive is faster by a factor of {non_recursive_duration/recursive_duration:.2f} x")
+
+# Results on my machine:
+#   With a fresh cache across 100 tries, non-recursive is consistently over 12 times faster.
+#   Keeping the cache across those tries, recursive is only 2 times faster.
+#   Separating the parsing out of the timing runs would yield a better comparison.
