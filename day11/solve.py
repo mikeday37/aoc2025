62cd59534f72611951cee851bbee8fc8c326c624
Mike Day <mikeday370146@gmail.com>
2026-03-14 22:46:36 -0400

    prepare equivalent but faster technique for part 2

--- day11/solve.py
+++ day11/solve.py
@@ -6,14 +6,51 @@ def parse_input(input):
         for device, *outputs in [line.split() for line in input.splitlines()]
     }
 
-def count_paths(start, graph):
+def count_paths(start, string_graph):
     def walk(at):
         if at == "out":
             return 1
-        return sum(walk(next) for next in graph[at])
+        return sum(walk(next) for next in string_graph[at])
     return walk(start)
 
-def solve_part_1(graph):
-    return count_paths("you", graph)
-    
-print("Part 1:", solve_part_1(parse_input(read_input())))
+def solve_part_1(string_graph):
+    return count_paths("you", string_graph)
+
+def get_canonical_graph(string_graph):
+    ids = {key: index for index, key in enumerate(string_graph)}
+    return [[ids[next] for next in string_graph[key] if next != "out"] for key in string_graph], ids
+
+def get_reverse_graph(graph):
+    n = len(graph)
+    reverse = [[] for _ in range(0, n)]
+    for id in range(0, n):
+        for next in graph[id]:
+            reverse[next].append(id)
+    return reverse
+
+def count_paths_backward(top_key, string_graph, setup = None, visitor = None):
+    graph, ids = get_canonical_graph(string_graph)
+    if setup:
+        setup(graph, ids)
+    back = get_reverse_graph(graph)
+    top = ids[top_key]
+    n = len(graph)
+    remaining = [len(graph[id]) for id in range(0, n)]
+    paths = [1 if remaining[id] == 0 else 0 for id in range(0, n)]
+    ready = set(id for id in range(0, n) if remaining[id] == 0)
+    while ready:
+        for id in tuple(ready):
+            ready.remove(id)
+            paths[id] += sum(paths[child] for child in graph[id])
+            if visitor:
+                visitor(id, graph, paths)
+            for parent in back[id]:
+                remaining[parent] -= 1
+                assert(remaining[parent] >= 0)
+                if remaining[parent] == 0:
+                    ready.add(parent)
+    return paths[top]
+
+def solve_part_2(string_graph):
+    return None
+
--- day11/test.py
+++ day11/test.py
@@ -1,6 +1,6 @@
 from common import *
 from .solve import *
-from .example import _example
+from .example import _example, _example2
 
 test(parse_input, {'a': ['b', 'c'], 'd': ['e']}, """\
     d: e
@@ -11,9 +11,38 @@ test(parse_input,
     {'aaa': ['you', 'hhh'], 'you': ['bbb', 'ccc'], 'bbb': ['ddd', 'eee'], 'ccc': ['ddd', 'eee', 'fff'], 'ddd': ['ggg'], 'eee': ['out'], 'fff': ['out'], 'ggg': ['out'], 'hhh': ['ccc', 'fff', 'iii'], 'iii': ['out']}
     , _example)
 
-_parsed_example = parse_input(_example)
-_parsed_input = parse_input(read_input())
+_parsed_test = parse_input("a: b\nb: c d\nc: out\nd: c")
+
+test(get_canonical_graph, (
+        [[1], [2,3], [], [2]], 
+        {"a": 0, "b": 1, "c": 2, "d": 3}
+    ), _parsed_test)
+
+test(get_reverse_graph, [[], [0], [1,3], [1]], get_canonical_graph(_parsed_test)[0])
+
+_test_def = """\
+    a: b c h i
+    b: d
+    c: d e
+    d: f g
+    e: out
+    f: g
+    g: out
+    h: c
+    i: h
+"""
+
+_parsed_example, _parsed_example2, _parsed_input, _parsed_test = [parse_input(e) for e in [_example, _example2, read_input(), _test_def]]
+
+test(count_paths, 11, "a", _parsed_test)
+test(count_paths, 5, "you", _parsed_example)
+test(count_paths, 8, "svr", _parsed_example2)
+
+test(count_paths_backward, 11, "a", _parsed_test)
+test(count_paths_backward, 5, "you", _parsed_example)
+test(count_paths_backward, 8, "svr", _parsed_example2)
 
 test(solve_part_1, 5, _parsed_example)
 
 verify_known_answer(solve_part_1, 688, _parsed_input)
+verify_known_answer(count_paths_backward, 688, "you", _parsed_input)
