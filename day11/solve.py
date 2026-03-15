0f20f603539ebb84a908c0d02724525b1b432a00
Mike Day <mikeday370146@gmail.com>
2026-03-15 11:44:38 -0400

    solved day 11 part 2

--- day11/solve.py
+++ day11/solve.py
@@ -1,4 +1,5 @@
 from common import *
+from timeit import timeit
 
 def parse_input(input):
     return {
@@ -16,6 +17,7 @@ def count_paths(start, string_graph):
 def solve_part_1(string_graph):
     return count_paths("you", string_graph)
 
+
 def get_canonical_graph(string_graph):
     ids = {key: index for index, key in enumerate(string_graph)}
     return [[ids[next] for next in string_graph[key] if next != "out"] for key in string_graph], ids
@@ -28,29 +30,81 @@ def get_reverse_graph(graph):
             reverse[next].append(id)
     return reverse
 
-def count_paths_backward(top_key, string_graph, setup = None, visitor = None):
-    graph, ids = get_canonical_graph(string_graph)
-    if setup:
-        setup(graph, ids)
-    back = get_reverse_graph(graph)
-    top = ids[top_key]
+def visit_graph_backward(graph, reverse, setup = None, visitor = None):
+    assert list(map(sorted, reverse)) == list(map(sorted, get_reverse_graph(graph))), "reverse must be the reverse of graph"
     n = len(graph)
     remaining = [len(graph[id]) for id in range(0, n)]
-    paths = [1 if remaining[id] == 0 else 0 for id in range(0, n)]
     ready = set(id for id in range(0, n) if remaining[id] == 0)
+    assert ready, "graph must have bottom nodes"
+    if setup:
+        setup(remaining)
     while ready:
         for id in tuple(ready):
             ready.remove(id)
-            paths[id] += sum(paths[child] for child in graph[id])
             if visitor:
-                visitor(id, graph, paths)
-            for parent in back[id]:
+                visitor(id)
+            for parent in reverse[id]:
                 remaining[parent] -= 1
                 assert(remaining[parent] >= 0)
                 if remaining[parent] == 0:
                     ready.add(parent)
-    return paths[top]
+    assert all(remaining[id] == 0 for id in range(0, n)), "all nodes must be visited"
+
+def count_fft_dac_paths(top_key, string_graph):
+    graph, ids = get_canonical_graph(string_graph)
+    reverse = get_reverse_graph(graph)    
+    n = len(graph)
+    below_fft, below_dac = [False] * n, [False] * n
+    above_fft, above_dac = [False] * n, [False] * n
+    paths = []
+
+    # 1st pass - mark all nodes above (or at) fft/dac
+    def mark_above(id):
+        if id == ids["fft"]:
+            above_fft[id] = True
+        else:
+            above_fft[id] = any(above_fft[child] for child in graph[id])
+        if id == ids["dac"]:
+            above_dac[id] = True
+        else:
+            above_dac[id] = any(above_dac[child] for child in graph[id])
+    visit_graph_backward(graph, reverse, None, mark_above)
+
+    # 2nd pass - mark all nodes below (or at) fft/dac
+    def mark_below(id):
+        if id == ids["fft"]:
+            below_fft[id] = True
+        else:
+            below_fft[id] = any(below_fft[parent] for parent in reverse[id])
+        if id == ids["dac"]:
+            below_dac[id] = True
+        else:
+            below_dac[id] = any(below_dac[parent] for parent in reverse[id])
+    visit_graph_backward(reverse, graph, None, mark_below)
+
+    # final pass - count paths that include both
+    def is_on_path(id):
+        return (above_fft[id] or below_fft[id]) and (above_dac[id] or below_dac[id])
+    def setup(remaining):
+        nonlocal paths
+        paths = [1 if remaining[id] == 0 and is_on_path(id) else 0 for id in range(0, n)]
+    def count(id):
+        paths[id] += sum(paths[child] for child in graph[id] if is_on_path(id))
+    visit_graph_backward(graph, reverse, setup, count)
+
+    return paths[ids[top_key]]
 
 def solve_part_2(string_graph):
-    return None
+    return count_fft_dac_paths("svr", string_graph)
 
+
+_parsed_input = parse_input(read_input())
+print("Part 1:", solve_part_1(_parsed_input))
+print("Part 2:", solve_part_2(_parsed_input))
+
+
+print()
+tries = 100
+part2_duration = timeit(lambda: solve_part_2(_parsed_input), number=tries) / tries
+print(f"Part 2 duration: {part2_duration:.6f} seconds")
+print(f"Part 2 answer with commas: {solve_part_2(_parsed_input):,}") # for reading convenience
--- day11/test.py
+++ day11/test.py
@@ -23,7 +23,7 @@ test(get_reverse_graph, [[], [0], [1,3], [1]], get_canonical_graph(_parsed_test)
 _test_def = """\
     a: b c h i
     b: d
-    c: d e
+    c: e d
     d: f g
     e: out
     f: g
@@ -38,11 +38,35 @@ test(count_paths, 11, "a", _parsed_test)
 test(count_paths, 5, "you", _parsed_example)
 test(count_paths, 8, "svr", _parsed_example2)
 
+def count_paths_backward(top_key, string_graph):
+    graph, ids = get_canonical_graph(string_graph)
+    reverse = get_reverse_graph(graph)
+    n = len(graph)
+    paths = []
+    def setup(remaining):
+        nonlocal paths
+        paths = [1 if remaining[id] == 0 else 0 for id in range(0, n)]
+    def visitor(id):
+        paths[id] += sum(paths[child] for child in graph[id])
+    visit_graph_backward(graph, reverse, setup, visitor)
+    return paths[ids[top_key]]
+
 test(count_paths_backward, 11, "a", _parsed_test)
 test(count_paths_backward, 5, "you", _parsed_example)
 test(count_paths_backward, 8, "svr", _parsed_example2)
 
+def is_double_reversible(string_graph):
+    graph = get_canonical_graph(string_graph)[0]
+    return list(map(sorted, graph)) == list(map(sorted, get_reverse_graph(get_reverse_graph(graph))))
+
+test(is_double_reversible, True, _parsed_test)
+test(is_double_reversible, True, _parsed_example)
+test(is_double_reversible, True, _parsed_example2)
+test(is_double_reversible, True, _parsed_input)
+
 test(solve_part_1, 5, _parsed_example)
+test(solve_part_2, 2, _parsed_example2)
 
 verify_known_answer(solve_part_1, 688, _parsed_input)
 verify_known_answer(count_paths_backward, 688, "you", _parsed_input)
+verify_known_answer(solve_part_2, 293263494406608, _parsed_input)
